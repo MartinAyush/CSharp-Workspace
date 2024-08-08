37522d29ca5fe8551fcc3539357d049c60ab47ee
Martin Anthony <martinayush04@gmail.com>
2024-08-08 22:51:09 +0530

    Added 2 more problems

---
+++ ConsoleApp/StriverDsa/OnlineStockSpanner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.StriverDsa
+{
+
+    public class StockSpanner
+    {
+        private Stack<Tuple<int, int>> _stocksBucket;
+        private int _idx = 0;
+        public StockSpanner()
+        {
+            _stocksBucket = new Stack<Tuple<int, int>>();
+        }
+
+        public int Next(int price)
+        {
+            int ans = -1;
+            while(_stocksBucket.Count > 0 && _stocksBucket.Peek().Item1 <= price)
+            {
+                _stocksBucket.Pop();
+            }
+
+            if(_stocksBucket.Count > 0)
+            {
+                ans = _idx - _stocksBucket.Peek().Item2;
+            }
+            else
+            {
+                ans = _idx - ans;
+            }
+
+            _stocksBucket.Push(new Tuple<int, int>(price, _idx));
+            _idx++;
+            return ans;
+        }
+    }
+}
--- ConsoleApp/StriverDsa/SlidingWindowMaximum.cs
+++ ConsoleApp/StriverDsa/SlidingWindowMaximum.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,25 +12,52 @@ namespace ConsoleApp.StriverDsa
     {
         public void Main()
         {
-
+            int[] nums = [1, 3, -1, -3, 5, 3, 6, 7];
+            int k = 3;
+            int[] ans = MaxSlidingWindow(nums, k);
+            foreach(int num in ans)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
         }
 
         public int[] MaxSlidingWindow(int[] nums, int k)
         {
             int len = nums.Length;
-            List<int> result = new List<int>();
-            LinkedList<int> deque = new LinkedList<int>();
-            
-            int max = int.MinValue;
+            LinkedList<int> maxValues = new LinkedList<int>();
+            int[] ans = new int[len - k + 1];
+
             for(int i = 0; i < k; i++)
             {
-                max = Math.Max(max, nums[i]);
+                while (maxValues.Count > 0 && nums[maxValues.Last.Value] <= nums[i])
+                {
+                    maxValues.RemoveLast();
+                }
+
+                maxValues.AddLast(i);
             }
+
+            ans[0] = nums[maxValues.First.Value];
 
             for(int i = k; i < len; i++)
             {
+                if(maxValues.First.Value == (i - k))
+                {
+                    maxValues.RemoveFirst();
+                }
 
+                while (maxValues.Count > 0 && nums[maxValues.Last.Value] <= nums[i])
+                {
+                    maxValues.RemoveLast();
+                }
+
+                maxValues.AddLast(i);
+
+                ans[i - k + 1] = nums[maxValues.First.Value];
             }
+
+            return ans;
         }
     }
 }
