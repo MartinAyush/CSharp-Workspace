f65a227224a9c72779b46b17bd18ebd7136fe693
Martin Anthony <martinayush04@gmail.com>
2024-08-12 17:46:56 +0530

    - Added more solved problems

---
+++ ConsoleApp/StriverDsa/AtoIOrStringToInt.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.StriverDsa
+{
+    public class AtoIOrStringToInt
+    {
+        public void Main()
+        {
+            string str = "2147483648";
+            int ans = MyAtoi(str);
+            Console.WriteLine(ans);
+        }
+
+        public int MyAtoi(string s)
+        {
+            if (string.IsNullOrEmpty(s) || string.IsNullOrWhiteSpace(s))
+            {
+                return 0;
+            }
+            s = s.Trim();
+            int len = s.Length;
+
+            const int maxValue = int.MaxValue - 1;
+            const int minValue = int.MinValue;
+
+            long ans = 0;
+            bool sign = false;
+            bool numberVisited = false;
+            bool signVisited = false;
+
+            for (int i = 0; i < len; i++)
+            {
+                if (!char.IsNumber(s[i]))
+                {
+                    if ((s[i] == '+' || s[i] == '-') && !numberVisited && !signVisited)
+                    {
+                        signVisited = true;
+                        sign = s[i] == '-' ? true : false;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    numberVisited = true;
+                    long cmp = ans * 10;
+                    if (cmp >= maxValue || cmp <= minValue)
+                    {
+                        return sign ? minValue : maxValue;
+                    }
+                    ans = cmp + (s[i] - '0');
+                }
+            }
+
+            ans = sign ? -1 * ans : ans;
+            if (ans >= maxValue || ans <= minValue)
+            {
+                return sign ? minValue : maxValue;
+            }
+            return (int)ans;
+        }
+    }
+}
---
+++ ConsoleApp/StriverDsa/MaximumOfMinimumForEveryWindowSize.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.StriverDsa
+{
+    public class MaximumOfMinimumForEveryWindowSize
+    {
+        public void Main()
+        {
+            int[] arr = { 10, 20, 30, 50, 10, 70, 30 };
+            int n = arr.Length;
+            List<int> ans = maxOfMin(arr, n);
+            foreach(var item in ans)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public int[] MaxMinWindowButeForce(int[] a, int n)
+        {
+            int[] ans = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                //ans[i] = MaxInMinWindow(a, n, i + 1);
+            }
+
+            return ans;
+        }
+
+        private int MaxInMinWindowButeForce(int[] arr, int arrLen, int windowSize)
+        {
+            // Find min in window
+            // For all min value return the max element
+            if(windowSize == 1)
+            {
+                return arr.Max();
+            }
+
+            List<int> minElements = new List<int>();
+            LinkedList<int> deque = new LinkedList<int>();
+
+            for(int i = 0; i < windowSize; i++)
+            {
+                while(deque.Count > 0 && arr[deque.Last.Value] > arr[i])
+                {
+                    deque.RemoveLast();
+                }
+                deque.AddLast(i);
+            }
+
+            minElements.Add(arr[deque.First.Value]);
+
+            for(int i = windowSize; i < arrLen; i++)
+            {
+                if(deque.First.Value == (i - windowSize))
+                {
+                    deque.RemoveFirst();
+                }
+
+                while(deque.Count > 0 && arr[deque.Last.Value] > arr[i])
+                {
+                    deque.RemoveLast();
+                }
+
+                deque.AddLast(i);
+                minElements.Add(arr[deque.First.Value]);
+            }
+            return minElements.Max();
+        }
+
+        public List<int> maxOfMin(int[] arr, int n)
+        {
+            // Find next and prev smaller idx's 
+            // Find the range in which they are smaller by [ next - prev - 1] here next and prev are idx's
+            // put the value in range [considering range - 1 as index]
+
+            int[] nextSmaller = GetNextSmallerElement(arr, n);
+            int[] prevSmaller = GetPrevSmallerElement(arr, n);
+
+            int[] ans = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                int range = (nextSmaller[i] - prevSmaller[i]) - 1;
+                ans[range - 1] = Math.Max(arr[i], ans[range - 1]);
+            }
+
+            // if the element if greater in larger window then its also larger in smaller window
+            // consider traversing backward because smaller window might not be filled
+            for(int i = n - 2; i >= 0; i--)
+            {
+                ans[i] = Math.Max(ans[i], ans[i + 1]);
+            }
+
+            return ans.ToList();
+        }
+
+        private int[] GetPrevSmallerElement(int[] arr, int n)
+        {
+            int[] ans = new int[n];
+            Stack<int> visitedIndex = new Stack<int>();
+
+            for(int i = 0; i < n; i++)
+            {
+                while(visitedIndex.Count > 0 && arr[visitedIndex.Peek()] >= arr[i])
+                {
+                    visitedIndex.Pop();
+                }
+
+                if(visitedIndex.Count > 0)
+                {
+                    ans[i] = visitedIndex.Peek();
+                }
+                else
+                {
+                    ans[i] = -1;
+                }
+
+                visitedIndex.Push(i);
+            }
+
+            return ans;
+        }
+
+        private int[] GetNextSmallerElement(int[] arr, int n)
+        {
+            int[] ans = new int[n];
+            Stack<int> visitedIndexes = new Stack<int>();
+
+            for(int i = n - 1; i >= 0; i--)
+            {
+                while(visitedIndexes.Count > 0 && arr[visitedIndexes.Peek()] >= arr[i])
+                {
+                    visitedIndexes.Pop();
+                }
+
+                if(visitedIndexes.Count > 0)
+                {
+                    ans[i] = visitedIndexes.Peek();
+                }
+                else
+                {
+                    ans[i] = n;
+                }
+
+                visitedIndexes.Push(i);
+            }
+
+            return ans;
+        }
+    }
+}
